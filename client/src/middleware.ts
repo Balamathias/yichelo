a38b03f23d46f169f86f2db7ae4c7c3bd2ddeb4b
Ladalo-Daniel <balaladalo@gmail.com>
2024-11-10 10:51:08 +0100

    Dashboard layout

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -6,24 +6,21 @@ export async function middleware(request: NextRequest) {
   const user = await getUser();
   const currentPath = request.nextUrl.pathname;
 
-  console.log('User:', user);
-
   if (!user && currentPath.startsWith('/dashboard')) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  try {
-    if (user?.roles && !user.roles.includes('admin') && currentPath.startsWith('/dashboard')) {
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
-  } catch (error) {
-    console.error('Error in middleware:', error);
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (user && (currentPath.startsWith('/login') || currentPath.startsWith('/register'))) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  if (user && currentPath.startsWith('/dashboard') && !user.roles?.includes('admin')) {
+    return NextResponse.redirect(new URL('/', request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*'],
+  matcher: ['/dashboard/:path*', '/login', '/register'],
 };
