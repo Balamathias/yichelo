2aae583d09ce4d981e2db215989952393652228f
Balamathias <balamathias05@gmail.com>
2024-11-28 13:06:33 +0100

    Resets

--- server/src/routes/recommendation.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { getRecommendedProducts } from "../controllers/recommendation.controller";
-
-const router = Router();
-
-router.get("/", getRecommendedProducts as any);
-
-export default router;
