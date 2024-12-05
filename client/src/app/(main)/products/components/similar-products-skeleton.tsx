0a9b12ce9839b5dea19c15132ea0dcb3f4c9a4d2
Balamathias <balamathias05@gmail.com>
2024-12-05 06:05:28 +0100

    Features

--- client/src/app/(main)/products/components/similar-products-skeleton.tsx
+++ client/src/app/(main)/products/components/similar-products-skeleton.tsx
@@ -6,7 +6,7 @@ const SimilarProductsSkeleton = () => {
     <div className='flex flex-col gap-y-4'>
       <div className='flex flex-col gap-y-4'>
         <Skeleton className='h-6 w-1/4' /> {/* Placeholder for section title */}
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5'>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-1.5 md:gap-5'>
           {Array.from({ length: 4 }).map((_, index) => ( // Adjust the number of skeletons as needed
             <div key={index} className='flex flex-col gap-y-2'>
               <Skeleton className='h-48 w-full' /> {/* Placeholder for product image */}
